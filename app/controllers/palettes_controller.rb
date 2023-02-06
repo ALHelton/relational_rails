1c6bf252a313fb3fdbe255b111e7b8ff3f374736
Andra Helton <andrahelton@gmail.com>
2023-02-05 20:20:49 -0700

    Add user story 11

--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -1,9 +1,25 @@
 class PalettesController < ApplicationController
+  
+  def new
+  end
+
   def index
     @palettes = Palette.all.order("created_at DESC")
   end
 
   def show
     @palette = Palette.find(params[:id])
+  end
+
+  def create
+    Palette.create(palette_params)
+    redirect_to '/palettes'
+  end
+  
+
+  private
+
+  def palette_params
+    params.permit(:name, :brand, :cartridge_capacity, :recyclable)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/palettes', to: 'palettes#index'
+  get '/palettes/new', to: 'palettes#new'
   get '/palettes/:id', to: 'palettes#show'
 
   get '/paints', to: 'paints#index'
   get '/paints/:id', to: 'paints#show'
 
   get '/palettes/:id/paints', to: 'palette_paints#index'
+  post '/palettes', to: 'palettes#create'
 end
